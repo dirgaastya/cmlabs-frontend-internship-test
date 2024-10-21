c708b75023608aac23cf6fac5ce40a5430384916
dirgaastya <astya444@gmail.com>
2024-10-22 01:57:38 +0700

    feat(Navbar): add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NAVIGATION_LINKS } from '../utils/constants/navbar.constant';
+import LogoApp from './LogoApp.vue';
+</script>
+<template>
+  <nav class="flex items-center justify-between py-6">
+    <LogoApp />
+
+    <ul class="flex items-center space-x-6">
+      <li v-for="(link, index) in NAVIGATION_LINKS" :key="index">
+        <router-link
+          :to="link.to"
+          class="text-base text-gray-900 hover:text-red-400 transition ease-in duration-300"
+          >{{ link.name }}</router-link
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ src/utils/constants/navbar.constant.ts
@@ -0,0 +1,11 @@
+import { NavMenu } from '../type/navbar.type.ts';
+export const NAVIGATION_LINKS: NavMenu[] = [
+  {
+    name: 'Home',
+    to: '/',
+  },
+  {
+    name: 'About',
+    to: '/about',
+  },
+];
