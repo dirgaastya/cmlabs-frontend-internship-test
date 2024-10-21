ba1dc20174af9b92aa0862b8bbbd270dee4d0e18
dirgaastya <astya444@gmail.com>
2024-10-22 02:21:23 +0700

    chore(tailwind): add hero image class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image':
+          'url("https://images.unsplash.com/photo-1533777324565-a040eb52facd?q=80&w=1436&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      },
+    },
   },
   plugins: [],
 };
