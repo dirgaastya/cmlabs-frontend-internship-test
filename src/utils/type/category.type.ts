de3e45d9b7a61f54a828947790f898f39fc87f57
dirgaastya <astya444@gmail.com>
2024-10-22 14:52:14 +0700

    feat: add category detail page

--- src/utils/type/category.type.ts
+++ src/utils/type/category.type.ts
@@ -5,8 +5,15 @@ export interface ICategory {
   strCategoryDescription: string;
 }
 
+export interface IMeals {
+  strMeal: string;
+  strMealThumb: string;
+  idMeal: string;
+}
+
 export interface ICategoriesStore {
   categories: ICategory[];
+  meals: IMeals[];
   loading: boolean;
   error: string;
 }
