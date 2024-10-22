f3b3d0f5c91ae6e18698e53bfc9097c6b3ce0ef4
dirgaastya <astya444@gmail.com>
2024-10-22 16:07:55 +0700

    feat: add route meal detail

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router';
 import Category from '../pages/category/Category.vue';
 import CategoryDetail from '../pages/category/CategoryDetail.vue';
+import MealDetail from '../pages/meals/MealDetail.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', component: Category },
   { path: '/:categoryName', component: CategoryDetail },
+  { path: '/:categoryName/:id', component: MealDetail },
 ];
 
 const router = createRouter({
