2902579306f57777580f7c186a7a6546ff748906
dirgaastya <astya444@gmail.com>
2024-10-22 16:08:24 +0700

    feat: add action to get meal by id

--- src/stores/category.store.ts
+++ src/stores/category.store.ts
@@ -1,11 +1,12 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
-import { ICategoriesStore } from '../utils/type/category.type';
+import { ICategoriesStore, IMealDetail } from '../utils/type/category.type';
 
 export const useCategoryStore = defineStore('categories', {
   state: (): ICategoriesStore => ({
     categories: [],
     meals: [],
+    meal: {} as IMealDetail,
     loading: false,
     error: '',
   }),
@@ -27,6 +28,18 @@ export const useCategoryStore = defineStore('categories', {
           `/api/api/json/v1/1/filter.php?c=${categoryName}`
         );
         this.meals = response.data.meals;
+      } catch (error) {
+        this.error = 'Error while fetching data';
+      }
+      this.loading = false;
+    },
+    async fetchMealById(id: string): Promise<void> {
+      this.loading = true;
+      try {
+        const response = await axios.get(
+          `/api/api/json/v1/1/lookup.php?i=${id}`
+        );
+        this.meal = response.data.meals[0];
       } catch (error) {
         this.error = 'Error while fetching data';
       }
