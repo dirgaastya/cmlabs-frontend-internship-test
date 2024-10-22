262ae1a30f2ae3efac9420048211f11b1d25219d
dirgaastya <astya444@gmail.com>
2024-10-22 13:21:50 +0700

    feat(category): add card component

---
+++ src/components/Category/Card.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ICategory } from '../../utils/type/category.type';
+
+interface CardProps {
+  category: ICategory;
+}
+
+defineProps<CardProps>();
+</script>
+<template>
+  <router-link
+    :to="`/category/${category.idCategories}`"
+    class="rounded-3xl h-32 overflow-hidden bg-white bg-no-repeat bg-center bg-cover group hover:-translate-y-2 transition-all ease-in duration-200"
+    :style="`background-image: url('${category.strCategoryThumb}');`"
+  >
+    <div class="bg-black/40 h-full py-6 px-6 flex items-center justify-center">
+      <h4
+        class="font-semibold text-white group-hover:text-red-400 group-hover:scale-105 transition-all ease-in duration-200"
+      >
+        {{ category.strCategory }}
+      </h4>
+    </div>
+  </router-link>
+</template>
+
+\
--- src/components/Category/List.vue
+++ src/components/Category/List.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ICategory } from '../../utils/type/category.type';
 import Loading from '../Loading.vue';
+import CategoryCard from './Card.vue';
+
 defineProps({
   data: {
     type: Array as () => ICategory[],
@@ -25,23 +27,11 @@ defineProps({
     </div>
     <transition name="fade">
       <div v-if="!loading" class="grid grid-cols-4 gap-x-8 gap-y-6">
-        <router-link
-          :to="`/category/${category.idCategories}`"
+        <CategoryCard
           v-for="(category, index) in data"
           :key="index"
-          class="rounded-3xl h-32 overflow-hidden bg-white bg-no-repeat bg-center bg-cover group hover:-translate-y-2 transition-all ease-in duration-200"
-          :style="`background-image: url('${category.strCategoryThumb}');`"
-        >
-          <div
-            class="bg-black/40 h-full py-6 px-6 flex items-center justify-center"
-          >
-            <h4
-              class="font-semibold text-white group-hover:text-red-400 group-hover:scale-105 transition-all ease-in duration-200"
-            >
-              {{ category.strCategory }}
-            </h4>
-          </div>
-        </router-link>
+          :category="category"
+        />
       </div>
     </transition>
   </section>
